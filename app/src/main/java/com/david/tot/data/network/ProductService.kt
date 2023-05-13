ac0ec915774d47c6eae43c5323894c65adba16e9
davidlab44 <davidlab44@gmail.com>
2023-05-13 18:39:55 -0500

    update-image-5

--- app/src/main/java/com/david/tot/data/network/ProductService.kt
+++ app/src/main/java/com/david/tot/data/network/ProductService.kt
@@ -17,25 +17,6 @@ class ProductService @Inject constructor(private val api:IProductApiClient) {
 
     suspend fun addProduct(product:Product):Int{
         return withContext(Dispatchers.IO) {
-            /*
-            val product:Product=Product(999, "kiwi","image.jpg","bal bla blazzzto",1000,0,0,1)
-
-            val jsonObject = JSONObject()
-            jsonObject.put("id", 999)
-            jsonObject.put("name", "")
-            jsonObject.put("image", "image.png")
-            jsonObject.put("description", "jac kasdfdasfj sdakjf sadlf jasjdf ")
-            jsonObject.put("price", 1000)
-            jsonObject.put("requested_amount", 0)
-
-            val jsonObjectString = jsonObject.toString()
-
-            // usar Gson Converter osea saltarse la siguiente linea
-            // NO ESTOY OBTENIENDO LA CADENA QUE RETORTNO DEL BAKEND
-             val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
-            // Do the POST request and get response
-            */
-
             val respuesta = api.addProduct(product)
             val respuestaBody =respuesta.body().toString()
             val respuestaBody2 = respuesta.body()
@@ -59,94 +40,17 @@ class ProductService @Inject constructor(private val api:IProductApiClient) {
 
     suspend fun updateProduct(product:Product){
         return withContext(Dispatchers.IO) {
-            //val product:Product=Product(13, "mojarra","public/tot/product/product-disabled.png","bal bla blazzzto",1000,0,0,1)
             val response = api.updateProduct(product.id.toString(),product)
             val res = response
         }
     }
 
-
     suspend fun uploadPicture(part:MultipartBody.Part){
         return withContext(Dispatchers.IO) {
-            //val product:Product=Product(13, "mojarra","public/tot/product/product-disabled.png","bal bla blazzzto",1000,0,0,1)
             val response = api.uploadPicture(part)
             val res = response
         }
     }
 
-
-
-
-
-
-
-
-
-
-
-
-    /*
-    fun rawJSON() {
-
-        // Create Retrofit
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .build()
-
-        // Create Service
-        val service = retrofit.create(api::class.java)
-
-        // Create JSON using JSONObject
-        val jsonObject = JSONObject()
-        jsonObject.put("name", "Mango")
-        jsonObject.put("description", "fjasdjflkdj askldjflksdaj sadjf sdj jlksadjflkdjsa")
-        jsonObject.put("image", "https://static9.depositphotos.com/1642482/1148/i/600/depositphotos_11489401-stock-photo-orange-fruit.jpg")
-        jsonObject.put("price", 4000)
-        jsonObject.put("requested_amount", 0)
-        jsonObject.put("is_milligram", 0)
-        jsonObject.put("is_unit", 1)
-
-        // Convert JSONObject to String
-        val jsonObjectString = jsonObject.toString()
-
-        // Create RequestBody ( We're not using any converter, like GsonConverter, MoshiConverter e.t.c, that's why we use RequestBody )
-        val requestBody = jsonObjectString.toRequestBody("application/json".toMediaTypeOrNull())
-
-        CoroutineScope(Dispatchers.IO).launch {
-            // Do the POST request and get response
-            val response = service.addProduct(requestBody)
-
-            withContext(Dispatchers.Main) {
-                if (response.isSuccessful) {
-                    Log.e("#01",""+response.toString())
-                    // Convert raw JSON to pretty JSON using GSON library
-                    val gson = GsonBuilder().setPrettyPrinting().create()
-                    /*
-                    val prettyJson = gson.toJson(
-                        JsonParser.parseString(
-                            response.body()
-                                ?.string() // About this thread blocking annotation : https://github.com/square/retrofit/issues/3255
-                        )
-                    )
-                    Log.d("Pretty Printed JSON :", prettyJson)
-
-                    val intent = Intent(this@MainActivity, DetailsActivity::class.java)
-                    intent.putExtra("json_results", prettyJson)
-                    this@MainActivity.startActivity(intent)
-
-                     */
-
-                } else {
-
-                    Log.e("RETROFIT_ERROR", response.code().toString())
-
-                }
-            }
-        }
-    }
-
-
-
-     */
 }
 
--- app/src/main/java/com/david/tot/ui/UpdateProductViewModel.kt
+++ app/src/main/java/com/david/tot/ui/UpdateProductViewModel.kt
@@ -1,9 +1,7 @@
 package com.david.tot.ui
 
-import android.content.Context
 import android.graphics.Bitmap
 import android.os.Build
-import android.os.Environment
 import android.util.Log
 import androidx.annotation.RequiresApi
 import androidx.compose.runtime.getValue
@@ -14,7 +12,6 @@ import androidx.lifecycle.ViewModel
 import com.david.tot.domain.UpdateImageProductUseCase
 import com.david.tot.domain.UpdateProductUseCase
 import com.david.tot.domain.model.Product
-import com.david.tot.util.ImageFile
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,7 +19,6 @@ import kotlinx.coroutines.launch
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
-import java.io.InputStream
 import javax.inject.Inject
 
 @HiltViewModel
@@ -66,11 +62,8 @@ class UpdateProductViewModel @Inject constructor(
 
     @RequiresApi(Build.VERSION_CODES.R)
     fun updateProductImage(bitmap: Bitmap){
-        //val byteArray = convertBitmapToFile(bitmap)
         CoroutineScope(Dispatchers.IO).launch {
-            //val byteArray: ByteArray? = ImageFile().encodeToBase64(bitmap)
             val file = bitmapToFile(bitmap,"gato.png")
-
             if (file != null) {
                 updateImageProductUseCase.invoke(file)
             }else{
@@ -80,85 +73,23 @@ class UpdateProductViewModel @Inject constructor(
     }
 
     @RequiresApi(Build.VERSION_CODES.R)
-    fun bitmapToFile(bitmap: Bitmap, fileNameToSave: String): File? { // File name like "image.png"
-        //create a file to write bitmap data
+    fun bitmapToFile(bitmap: Bitmap, fileNameToSave: String): File? {
         var file: File? = null
         var bitmapdata = byteArrayOf(0x2E, 0x38)
         return try {
-            //file = File(Environment.getExternalStorageDirectory().toString() + File.separator + fileNameToSave)
-            //TODO ojo Environment.getStorageDirectory() requires min sdk 30
-            //file = File(Environment.getStorageDirectory().toString() + File.separator + fileNameToSave)
-            val path = System.getProperty("user.dir")
-            Log.e("Actual directory",path )
             file = File("/storage/self/primary/Download" + File.separator + fileNameToSave)
-            Log.e("#001",Environment.getStorageDirectory().toString() + File.separator + fileNameToSave)
-            Log.e("Environment.getStorageDirectory().toString()",Environment.getStorageDirectory().toString() + File.separator + fileNameToSave)
-            Log.e("File.separator",File.separator)
-            Log.e("filename",fileNameToSave)
-            Log.e("Environment.getExternalStorageDirectory().toString() ",Environment.getExternalStorageDirectory().toString() )
-
             file.createNewFile()
-
-
-            //Convert bitmap to byte array
             val bos = ByteArrayOutputStream()
-            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos) // YOU can also save it in JPEG
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0, bos)
             bitmapdata = bos.toByteArray()
-
-            //write the bytes in file
             val fos = FileOutputStream(file)
             fos.write(bitmapdata)
             fos.flush()
             fos.close()
-            //file
-                //val isAnImage = ImageFile.decodeBase64("")
-            val isAnImage = ImageFile().isAnImage("/storage/self/primary/Download/gato.png")
-            val isAnImage2 = isAnImage
             file
         } catch (e: Exception) {
             e.printStackTrace()
-            file // it will return null
+            file
         }
     }
 }
-
-
-/*
-
-class AddProductViewModel @Inject constructor(private val addProductUseCase: AddProductUseCase) : ViewModel() {
-
-    var productName by mutableStateOf<String>("")
-    var productDescription by mutableStateOf<String>("")
-    var productPrice by mutableStateOf<Int>(0)
-    fun addProduct(){
-        if(productName.trim().length>1&&productDescription.trim().length>1&&productPrice>1){
-            val product = Product(1001,productName,"public/tot/product/product-disabled.png", productDescription,productPrice,0,0,1)
-            CoroutineScope(Dispatchers.IO).launch {
-                addProductUseCase.invoke(product)
-            }
-        }
-    }
-
-
-    //var recipeModel by mutableStateOf<List<Product>>(emptyList())
-    fun onCreate() {
-        //viewModelScope.launch {
-        CoroutineScope(Dispatchers.IO).launch {
-            //ProductRepository().requestProductList()
-            val product = Product(999,"Espinaca","fruiit.jpg", "glu glu glu",10000,0,0,1)
-            addProductUseCase.invoke(product)
-        }
-    }
-
-
-    fun addProduct(product:Product){
-        //TODO mostrar lo que retorna el producto creado  o por lo menos un aconfirmacion visual para  el usuario de que si se creo el producto
-        CoroutineScope(Dispatchers.IO).launch {
-            addProductUseCase.invoke(product)
-        }
-    }
-
-
-}
-
- */
