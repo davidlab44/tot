cddb5b2b4de333c18c90ba62759ee18336b501bb
davidlab44 <davidlab44@gmail.com>
2023-05-13 19:52:43 -0500

    updating image without idProduct

--- app/src/main/java/com/david/tot/util/constants.kt
+++ app/src/main/java/com/david/tot/util/constants.kt
@@ -3,13 +3,7 @@ package com.david.tot.util
 
 //const val MAP_MARKER ="https://www.google.com/maps/search/?api=1&query=36.26577,-92.54324"
 const val MAP_MARKER ="https://www.google.com/maps/search/?api=1&query"
-//const val BASE_URL = "http://192.168.216.234/auth/public/api/"
-//const val BASE_URL = "http://192.168.216.234/auth/public/api/"
-//const val BASE_URL = "http://192.168.43.86/auth/public/api/"
-const val BASE_URL = "http://192.168.43.234/auth/public/api/"
-//const val IMAGE_BASE_URL = "http://192.168.216.234/auth/"
-//const val IMAGE_BASE_URL = "http://192.168.43.86/auth/"
-const val IMAGE_BASE_URL = "http://192.168.43.234/auth/"
-fun convertBitmapToInputStream(){
-
-}
+const val IP_ADDRESS = "192.168.43.86"
+//const val IP_ADDRESS = "192.168.43.234"
+const val BASE_URL = "http://$IP_ADDRESS/auth/public/api/"
+const val IMAGE_BASE_URL = "http://$IP_ADDRESS/auth/"
